c89afcad159d25d3ed7428b3ca004fa0c122575e
Shuvo <74643721+AhmdShuvo@users.noreply.github.com>
2021-11-10 02:09:55 +0600

    3rd

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 app.use(cors())
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_pass}@cluster0.0qtlc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0qtlc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
