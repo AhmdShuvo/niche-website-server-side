1c17669bf4ac1282764a30cde14501f58ebc4bef
Shuvo <74643721+AhmdShuvo@users.noreply.github.com>
2021-11-11 19:28:29 +0600

    delete operation

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require("mongodb");
+const Objectid=require('mongodb').ObjectId;
 const express=require('express');
 const cors=require('cors');
 require("dotenv").config();
@@ -28,6 +29,7 @@ async function run() {
       const database = client.db('Cars-Server');
       const servicesCollection = database.collection('Cars');
       const purchaseCollection = database.collection('purchases');
+      const UsersCollection = database.collection('users');
 
 
 
@@ -37,7 +39,68 @@ async function run() {
            const result=await purchaseCollection.insertOne(order);
 res.send(result)
       });
+                    // Store Users ///
+
+
+       app.post('/users',async (req,res)=>{
+        const user=req.body;
+
+        const result=await UsersCollection.insertOne(user);
+
+        res.send(result)
+       
+       })
+
+      //  Update For Google USers///
+      app.put('/users',async(req,res)=>{
+
+        const user=req.body;
+
+        const filter = { email:user.email};
+        const options = { upsert: true };
+        const updateDoc = {$set:user};
+      const result= await  UsersCollection.updateOne(filter,updateDoc,options);
+
+       res.send(result)
+      })
+
+
+         //  Delete User Purchase //
+
+         app.delete("/order/:email",async(req,res)=>{
+          const id=req.params.email
+
+          const query={_id: Objectid(id)}
+          const result = await purchaseCollection.deleteOne(query);
+          res.send(result)
+          console.log(userEmail);
+       })
                 
+
+
+            //  Get User Orders //
+
+            app.get("/order/:email",async(req,res)=>{
+               const userEmail=req.params.email;
+
+               const query = {email: userEmail};
+               const cursor= purchaseCollection.find(query);
+               const result=await cursor.toArray()
+               res.json(result);
+            })
+
+
+         
+      // Get All USers //
+
+      app.get('/users',async(req,res)=>{
+
+        const cursor =UsersCollection.find({});
+
+          const result=await cursor.toArray()
+
+          res.json(result)
+      })
       // GEt All Cars ///
                 
          app.get('/cars',async(req,res)=>{
